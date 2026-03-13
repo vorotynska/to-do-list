406a890148e0a3aeaabcded605743b53c82e544e
Iryna Vorotynska <irynavorotynska@Mac.home>
2026-03-13 21:42:44 +0100

    create function handleListClick and function clearComleted

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # to-do-list
+
+localStorege
+event delegation
--- list.js
+++ list.js
@@ -2,53 +2,28 @@
 const input = document.getElementById("taskInput");
 const addBtn = document.getElementById('addBtn');
 const taskList = document.getElementById("taskList");
+const all = document.getElementById('all');
+const active = document.getElementById('active');
+const completed = document.getElementById('completed');
+const clearBtn = document.getElementById('clearBtn');
+
 //localStorage.removeItem('tasks');
 // Loading tasks from localStorage
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-console.log(tasks)
-renderTasks();
+
+renderTasks()
 
 addBtn.addEventListener('click', addTask);
 input.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') addTask();
 });
+taskList.addEventListener('click', handleListClick);
+clearBtn.addEventListener('click', clearCompleted);
 
 // Save function
 function saveTasks() {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
-
-// Task display function
-function renderTasks() {
-    taskList.innerHTML = '';
-    tasks.forEach((task, index) => {
-        const li = document.createElement('li');
-        li.textContent = task.text;
-
-        if (task.completed) {
-            li.classList.add('completed');
-        }
-
-        li.addEventListener('click', () => {
-            tasks[index].completed = !tasks[index].completed;
-            saveTasks();
-            renderTasks();
-        });
-
-        const deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'Delete';
-
-        deleteBtn.addEventListener('click', () => {
-            tasks.splice(index, 1);
-            saveTasks();
-            renderTasks();
-        });
-
-        li.appendChild(deleteBtn);
-        taskList.appendChild(li);
-    });
-}
-
 
 // Add task
 function addTask() {
@@ -56,37 +31,75 @@ function addTask() {
     if (text === '') return alert('Ensure you input a value in field');
 
     tasks.push({
-        text: text,
+        text,
         completed: false
     });
 
     saveTasks();
     renderTasks();
     input.value = '';
+    input.focus();
 }
 
-
-/*
-function addTask1() {
-    if (input.value.trim() === '') {
-        alert('Ensure you input a value in field')
-    } else {
+// Task display function
+function renderTasks() {
+    taskList.innerHTML = '';
+    tasks.forEach((task, index) => {
         const li = document.createElement('li');
-        taskList.appendChild(li);
-        li.textContent = input.value.trim();
-        li.addEventListener('click', () => {
-            li.classList.toggle('completed');
-        });
+
+        li.dataset.index = index;
+        li.textContent = task.text;
+
+        if (task.completed) {
+            li.classList.add('completed');
+        }
 
         const deleteBtn = document.createElement('button');
-        deleteBtn.innerText = 'Delete';
-        deleteBtn.setAttribute('class', 'deleteBtn');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.className = 'deleteBtn'
+
         li.appendChild(deleteBtn);
-        deleteBtn.addEventListener('click', () => {
-            li.remove();
-        })
+        taskList.appendChild(li);
+    });
+    infoTasks();
+}
+
+// One handler insted of many
+function handleListClick(e) {
+    const li = e.target.closest('li');
+    if (!li) return;
+
+    const index = li.dataset.index;
+
+    if (e.target.classList.contains('deleteBtn')) {
+        tasks.splice(index, 1);
+    } else {
+        tasks[index].completed = !tasks[index].completed
     }
 
-    input.value = '';
+    saveTasks();
+    renderTasks();
 }
-*/
+
+function clearCompleted() {
+    tasks.forEach((task, index) => {
+        if (task.completed) {
+            tasks.splice(index, 1);
+        }
+    })
+    clearBtn.style.display = 'none';
+    saveTasks();
+    renderTasks();
+}
+
+// General information about tasks
+function infoTasks() {
+    let completedTasks = tasks.filter((task) => task.completed);
+    let activeTasks = tasks.length - completedTasks.length;
+
+    all.textContent = `All: ${tasks.length}`;
+    completed.textContent = `Completed: ${completedTasks.length}`;
+    active.textContent = `Active: ${activeTasks}`;
+
+    if (completedTasks.length > 0) clearBtn.style.display = 'block';
+}
